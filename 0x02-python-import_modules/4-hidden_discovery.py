40b4954c24d5274df480a320c1e115675e38d3e3
m0imaritim <moimaritim@gmail.com>
2024-01-04 22:31:52 -0800

    initializing fix

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import hidden_4
-    for i in dir(hidden_4[]):
-        if i[0] != "__":
-            print(i)
+    names = sorted(dir(hidden_4[]))
+    for name in names:
+        if name[0] != '__0':
+            print(name)
