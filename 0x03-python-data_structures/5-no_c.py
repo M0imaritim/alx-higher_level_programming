4a36b339bbfb3c1476408d041df5216ce0e2c349
m0imaritim <moimaritim@gmail.com>
2024-01-08 11:55:59 -0800

    c me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    if my_string is None:
+        return
     new_str = list(my_string)
     for i in new_str:
-        if i == 'c' or i == 'C':
+        if i == 'c' and i == 'C':
             new_str.remove(i)
     return ''.join(new_str)
