a6434e8ad25a8f284bf41d36a44f6462851aeb35
m0imaritim <moimaritim@gmail.com>
2024-01-10 06:47:24 -0800

    difference

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -3,4 +3,5 @@ def only_diff_elements(set_1, set_2):
     if set_1 is None or set_2 is None:
         return
     set_3 = set_1.difference(set_2)
-    return set_3
+    set_4 = set_2 - set_1
+    return set_3 | set_4
