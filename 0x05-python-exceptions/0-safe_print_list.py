2a7db73cbe7a14a022fca7fe6195679aede3935a
m0imaritim <moimaritim@gmail.com>
2024-01-21 23:46:22 -0800

    errors and exceptions

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        printed = 0
+        for i in my_list[:x]:
+            print(i, end='')
+            printed += 1
+        print()
+        return printed
+    except IndexError:
+        printed = 0
+        for i in my_list[:]:
+            print(i, end='')
+            printed += 1
+        print()
+        return printed
