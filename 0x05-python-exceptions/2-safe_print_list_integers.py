305799578890e842ac3c1d404f4fcbaed150f092
m0imaritim <moimaritim@gmail.com>
2024-01-22 04:58:26 -0800

    index fix

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    if my_list is None:
-        my_list = []
     printed = 0
     for i in range(0, x):
         try:
             print("{:d}".format(my_list[i]), end='')
-        except (TypeError, ValueError, IndexError):
-            pass
-        else:
             printed += 1
+        except (TypeError, ValueError):
+            pass
     print()
     return printed
