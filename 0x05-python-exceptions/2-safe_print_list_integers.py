502ac6d2efaa12b5751e8eab7af377b2443de7a4
m0imaritim <moimaritim@gmail.com>
2024-01-22 04:21:38 -0800

    only integers safely

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    if my_list is None:
+        my_list = []
+    printed = 0
+    for i in my_list[:x]:
+        try:
+            print("{:d}".format(i), end='')
+        except:
+            continue
+        else:
+            printed += 1
+    print()
+    return printed
