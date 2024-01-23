9087dae261206519bb8c1ffc6d20ce0173880f68
m0imaritim <moimaritim@gmail.com>
2024-01-23 10:25:45 -0800

    square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''This is a class square that defines a square'''
+
+
+class Square:
+    '''instantiating size as a private attribute'''
+    def __init__(self, size):
+        self.__size = size
