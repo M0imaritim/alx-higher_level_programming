71a4795876098a0e9c989e54d4eba56bea5ab427
m0imaritim <moimaritim@gmail.com>
2024-01-23 11:12:25 -0800

    area of square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''A class square that defines a square'''
+
+
+class Square:
+    '''Instantiating a private attribute size'''
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        return self.__size ** 2
