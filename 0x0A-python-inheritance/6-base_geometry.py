21a97b2faa2eaaef812648f35f4d95c717821246
m0imaritim <moimaritim@gmail.com>
2024-02-05 12:18:10 -0800

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""module contains a class"""
+
+
+class BaseGeometry:
+    """This is a class with no implementation"""
+    def area(self):
+        """function with no implementation"""
+        raise Exception("area() is not implemented")
