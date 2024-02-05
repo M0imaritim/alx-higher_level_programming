4decc240e28c7dd791bfbd226049c0a79d1e4c5a
m0imaritim <moimaritim@gmail.com>
2024-02-05 12:28:12 -0800

    Integer validator

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""module contains a class"""
+
+
+class BaseGeometry:
+    """This is a class with two methods"""
+    def area(self):
+        """function with no implementation"""
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        """function that validates value"""
+        if value not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if self.value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
