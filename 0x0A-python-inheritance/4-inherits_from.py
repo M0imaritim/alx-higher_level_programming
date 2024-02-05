b4103e8d5a7026d3c7b2cf135557ad4a143cc3b8
m0imaritim <moimaritim@gmail.com>
2024-02-05 12:01:16 -0800

    quick fix

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -3,6 +3,6 @@ def inherits_from(obj, a_class):
     """
     returns True or False if object is an instance of a class
     """
-    if isinstance(obj, a_class):
+    if type(obj) is a_class:
         return False
-    return type(obj) is a_class
+    return isinstance(obj, a_class)
