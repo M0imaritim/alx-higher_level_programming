cd3e2993ff64732f6a334967978789510a756287
m0imaritim <moimaritim@gmail.com>
2024-02-07 12:07:08 -0800

    fix

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -15,11 +15,11 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.__height = height
 
-        def area(self):
-            """ returns the area of the instance"""
-            return self.__width * self.__height
+    def area(self):
+        """ returns the area of the instance"""
+        return self.__width * self.__height
 
-        def __str__(self):
-            """Returns string of implementation"""
-            return "[Rectangle] {:d}/{:d}"
+    def __str__(self):
+        """Returns string of implementation"""
+        return "[Rectangle] {:d}/{:d}"
                        .format(self.__width, self.__height)
