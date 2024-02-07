dc7e0678d26769b74338de145ca580cde4536112
m0imaritim <moimaritim@gmail.com>
2024-02-07 11:47:35 -0800

    fix

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -11,9 +11,9 @@ def append_after(filename="", search_string="", new_string=""):
     """
     with open(filename, 'r', encoding="utf-8") as f:
         lines = f.readlines()
+        for line in lines:
+            if line.find(search_string) != -1:
+                lines += [new_string]
 
     with open(filename, 'w', encoding="utf-8") as f:
-        for line in lines:
-            f.write(line)
-            if search_string in line:
-                f.write(new_string + '\n')
+        f.write("".join(lines))
