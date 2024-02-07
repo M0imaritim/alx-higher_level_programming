7f22ba7c59acd2e530001463695ef9106f5bafe3
m0imaritim <moimaritim@gmail.com>
2024-02-07 12:21:27 -0800

    Square #1

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """This module inherits from 9-rectangle.py"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
     """Class square inherits from superclass Rectangle"""
-
 
     def __init__(self, size):
         """Instantiation with size"""
