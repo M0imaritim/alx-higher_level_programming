7e739cc99439db3003204488cf59f9a35a9d26ae
m0imaritim <moimaritim@gmail.com>
2024-03-14 19:21:46 -0700

    upeercase fix

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,4 +1,4 @@
 -- script that lists all shows from hbtn_0d_tvshows_rate by their rating
-SELECT tv_shows.title, SUM(tv_show_ratings.rate) as rating FROM tv_shows
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating FROM tv_shows
 JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
 GROUP BY tv_shows.title ORDER BY rating DESC;
