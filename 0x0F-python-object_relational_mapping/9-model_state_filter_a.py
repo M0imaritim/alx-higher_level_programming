ecde342d031a5b811943329483a9a6ffbec5bb4e
m0imaritim <moimaritim@gmail.com>
2024-04-13 10:20:50 -0700

    style fix

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -11,8 +11,8 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == '__main__':
     url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(argv[1],
-                                                            argv[2],
-                                                            argv[3])
+                                                           argv[2],
+                                                           argv[3])
     engine = create_engine(url, pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
