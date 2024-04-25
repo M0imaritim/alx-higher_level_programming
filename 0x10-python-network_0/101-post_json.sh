c620269a384801da34bdcc864089ef60a037a5e7
m0imaritim <moimaritim@gmail.com>
2024-04-25 13:36:16 -0700

    H fix

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # ends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl -s -X post -d "$(cat "$2")" "$1"
+curl -s -X post -d "$(cat "$2")" -H "Content-Type: application/json" "$1"
