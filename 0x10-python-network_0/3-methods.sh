d2128835c7585c5f4808d50014a3f91fc1f6bec5
m0imaritim <moimaritim@gmail.com>
2024-04-25 11:48:37 -0700

    fix

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL and displays all HTTP methods the server will accept
-curl -s -I "$1" | grep -i "Allow"
+curl -s -I "$1" | grep -i "^Allow:" | cut -c 8-
