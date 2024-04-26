14d6d042036dedfeea060d3ae172eeea6d141f29
m0imaritim <moimaritim@gmail.com>
2024-04-26 12:14:04 -0700

    style fix

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
     try:
         with urllib.request.urlopen(url) as response:
-            result = response.read().decode('utf-8'))
+            result = response.read().decode('utf-8')
             print(result)
     except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
