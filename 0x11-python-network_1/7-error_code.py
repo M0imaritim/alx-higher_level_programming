54edaf9823757c4192cfd8da4972099a1d0f89d3
m0imaritim <moimaritim@gmail.com>
2024-04-26 14:03:28 -0700

    Error code #1

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""takes in a URL, sends a request to the URL and displays the
+body of the response"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    r = requests.get(url)
+    rc = r.status_code
+    if rc > 400 or rc == 400:
+        print("Error code: {}".format(rc))
