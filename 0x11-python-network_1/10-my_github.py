6f54c7b8e1f51a50598e2f9fae11be058c4c8661
m0imaritim <moimaritim@gmail.com>
2024-04-26 15:30:37 -0700

    git api

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" takes your GitHub credentials (username and password) and uses the GitHub
+API to display your id"""
+
+import sys
+import requests
+
+if __name__ == ""__main__:
+    user = sys.argv[1]
+    psswd = sys.argv[2]
+    url = "https://api.github.com/user"
+
+    auth = (user, psswd)
+
+    r = requests.get(url, auth=auth)
+
+    user_data = r.json()
+    print(user_data['id'])
