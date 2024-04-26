7eb89596def6909cd074a917798d35a9d2ddd02e
m0imaritim <moimaritim@gmail.com>
2024-04-26 08:13:37 -0700

    post email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""takes in a URL and an email, sends a POST request to the passed URL with the
+email as a parameterr, and displays the body of the response (decoded in utf-8)
+"""
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    e_data = urllib.parse.urlencode({'Your email is': email})
+    e_data = e_data.encode('utf-8')
+    with urllib.request.urlopen(url, data=e_data) as response:
+        page = response.read().decode('utf-8')
+    print(page)
