590dbf061731268ef78e7ebc43d8eb80e5198a88
m0imaritim <moimaritim@gmail.com>
2024-04-26 13:01:04 -0700

    style fix

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,7 +6,6 @@ if __name__ == "__main__":
     r = requests.get("https://alx-intranet.hbtn.io/status")
     content = r.text
 
-
     print("Body response:")
     print("\t- type: {}".format(type(content)))
     print("\t- content: {}".format(content))
