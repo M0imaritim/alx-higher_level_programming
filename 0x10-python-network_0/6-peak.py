4ebddc3331fb7180e6376058c56d770d9549cece
m0imaritim <moimaritim@gmail.com>
2024-04-28 20:11:20 -0700

    fix index error

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+""" function that finds a peak in a list of unsorted integers."""
 def find_peak(list_of_integers):
-    """ function that finds a peak in a list of unsorted integers."""
     lt = list_of_integers
-    for i in lt[0:]:
-        if (i - lt[i-1] == 0 or i - lt[i-1] > 0) and (i - lt[i+1] == 0 or i - lt[i+1] > 0):
-            return i
-        else:
-            return None
+    try:
+        for i in lt[0:]:
+            if (i - lt[i-1] >= 0) or (i - lt[i+1] >= 0):
+                return i
+            else:
+                return None
+    except IndexError:
+        return None
