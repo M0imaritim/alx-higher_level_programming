9e9805819daee25185013c57b6f6957630aa12ba
m0imaritim <moimaritim@gmail.com>
2024-05-22 19:22:00 -0700

    ends with 18

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const request = require('request');
+
+const apiUrl = process.argv[2];
+const characterId = '18';
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const films = JSON.parse(body).results;
+    let count = 0;
+
+    films.forEach(film => {
+      if (film.characters.includes(`https://swapi-api.alx-tools.com/ai/people/${characterId}/`)) {
+        count++;
+      }
+    });
+
+    console.log(count);
+  }
+});
