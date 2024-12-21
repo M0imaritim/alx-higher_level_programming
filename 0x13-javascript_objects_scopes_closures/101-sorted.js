36ca6f330e4e198133d5965684ab9181489667c8
moimaritim <moimaritim@gmail.com>
2024-12-22 00:28:34 +0300

    sorted

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const dict = require('./101-data').dict;
+const nnew = {};
+
+Object.keys(dict).map(function (key, index) {
+  if (nnew[dict[key]] === undefined) {
+    nnew[dict[key]] = [];
+  }
+  nnew[dict[key]].push(key);
+});
+
+console.log(nnew);
